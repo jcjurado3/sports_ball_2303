b5faecae884b6f17434b32ce9aee130e2e7bb766
Jimmy Jurado <jurado140@gmail.com>
2023-03-29 11:57:36 -0400

    Add working iteration 1.

---
+++ lib/player.rb
@@ -0,0 +1,32 @@
+class Player
+  attr_reader :name, :monthly_month, :nickname
+
+  def initialize(name, monthly_cost, contract_length)
+    @name = name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @nickname = nil
+  end
+
+  def first_name
+    name.split[0]
+  end
+  def last_name
+    name.split[1]
+  end
+
+  def monthly_cost
+    @monthly_cost
+  end
+
+  def contract_length
+    @contract_length
+  end
+  def total_cost
+    monthly_cost * contract_length
+  end
+  def set_nickname!(n_name)
+    @nickname = n_name
+  end
+
+end
